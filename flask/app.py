765e52c103482d266541d5cb36a9f553c6c5fc66
jjw1707 <jjw1707@gmail.com>
2014-10-25 01:39:06 -0700

    have templates working with layout.

--- flask/app.py
+++ flask/app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def index():
   return render_template("index.html")
 
+@app.route("/about")
+def about():
+  return render_template("about.html")
+
 @app.route("/hello/")
 def hello():
   return "Hello World!"
