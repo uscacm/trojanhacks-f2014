adfa96c7fc019eca4f508e86c4cc9703f60cca87
jjw1707 <jjw1707@gmail.com>
2014-10-24 23:28:32 -0700

    rendering home page without css.

--- flask/app.py
+++ flask/app.py
@@ -1,9 +1,18 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 @app.route("/")
+def index():
+  return render_template("index.html")
+
+@app.route("/hello/")
 def hello():
   return "Hello World!"
- 
+
+@app.route("/hello/<name>")
+def hello_name(name):
+  return "Hello %s!" % name
+
 if __name__ == "__main__":
-  app.run()
+  app.run(debug=True)
