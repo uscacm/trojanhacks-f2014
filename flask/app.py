5f30c9337634ba2bbd9deb433f8fd62426863bdd
jjw1707 <jjw1707@gmail.com>
2014-10-25 03:49:51 -0700

    schema and connecting to the db

--- flask/app.py
+++ flask/app.py
@@ -2,7 +2,27 @@ from flask import Flask
 from flask import render_template
 from flask import url_for
 from flask import request
+import sqlite3
+from contextlib import closing
+
+# Configuration
+DATABASE = "/tmp/todo.db"
+DEBUG = True
+SECRET_KEY = ""
+USERNAME = "admin"
+PASSWORD = "default"
+
 app = Flask(__name__)
+app.config.from_object(__name__)
+
+def connect_db():
+  return sqlite3.connect(app.config["DATABASE"])
+
+def init_db():
+  with closing(connect_db()) as db:
+    with app.open_resource('schema.sql', mode='r') as f:
+      db.cursor().executescript(f.read())
+    db.commit()
 
 @app.route("/")
 def index():
---
+++ flask/schema.sql
@@ -0,0 +1,5 @@
+drop table if exists tasks;
+create table tasks (
+  id integer primary key autoincrement,
+  name text not null
+);
